1a7cf7ba9888611a14b95d37e6ce860146ef1591
Lucas-Beni <lucgarcbeni@gmail.com>
2025-08-27 14:47:19 -0300

    criadas views FBV e CBV para a listagem de heróis

--- heroes/urls.py
+++ heroes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('hello/', views.hello_heroes, name='hello_heroes'),
-    path('lista/', views.lista_herois, name='lista_herois'),
+    path('hello/', hello_heroes, name='hello_heroes'),
+    path('lista/', lista_herois, name='lista_herois'),
+    path('cbv-lista/', HeroListView.as_view(), name='cbv_lista_herois'),
 ]
